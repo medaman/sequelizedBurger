a8cf0281425121c42817e78d2bcd2ca6e6356de3
Adam <medaman@gmail.com>
2017-10-05 00:40:14 -0700

    troubleshooting

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var port = process.env.PORT || 3000;
 
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+//app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
